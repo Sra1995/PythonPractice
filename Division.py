e18c9578be896585883e8740e967cd2b5aceba05
Sra1995 <109440699+Sra1995@users.noreply.github.com>
2023-02-28 08:05:06 -0500

    Add files via upload

---
+++ Division.py
@@ -0,0 +1,7 @@
+def Minus(a, b):
+    return (a / b)
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+print(f' {a} divided by {b} is {Minus(a, b)}')
---
+++ multipication.py
@@ -0,0 +1,7 @@
+def T(a, b):
+    return (a * b)
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+print(f'multipication of {a} times {b} is {T(a, b)}')
