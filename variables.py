bea5cff73014a224152e5dfb7aeca72fed176f91
Sra1995 <109440699+Sra1995@users.noreply.github.com>
2023-03-01 13:06:16 -0500

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,15 @@
+x = int(10.4) # in will make it int 
+y = float(3) # float will make it 3.0
+z = int(3.5) # in will make it int 
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+if x > y:
+    print(f'{x} is greater than {y}')
+    
+    
+if x > z:
+    print(f'{x} is greater than {z}')
