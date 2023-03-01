c4d92866dd62e248035d163b0c092073a14cd9a7
Sra1995 <109440699+Sra1995@users.noreply.github.com>
2023-03-01 13:22:36 -0500

    Add files via upload

---
+++ unpack_collection.py
@@ -0,0 +1,6 @@
+groceries = ["Chicken breast", "K-cups","ground Lamb", "Salmon Fillets","Oatmeal", "Spices"]
+a, b, c, d, e, f = groceries
+print(groceries)
+print(f'found {a}, {b}, {d}, {e}')
+print(f'{c} not available')
+print(f'{f} not available')
