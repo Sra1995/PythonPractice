7cc14249b97fc5b201cf4ceff19f107675c49681
Sra1995 <109440699+Sra1995@users.noreply.github.com>
2023-03-13 16:36:35 -0400

    Add files via upload

---
+++ Google IT Automation with Python Professional Certificate/Crash course python/fractions_question.py
@@ -0,0 +1,16 @@
+def fractional_part(numerator, denominator):
+    # Operate with numerator and denominator to
+    # keep just the fractional part of the quotient 
+    if denominator == 0 :
+        part = 0
+    else:
+        part = (numerator %denominator)/denominator
+    return part
+
+
+print(fractional_part(5, 5)) # Should print 0
+print(fractional_part(5, 4)) # Should print 0.25
+print(fractional_part(5, 3)) # Should print 0.66...
+print(fractional_part(5, 2)) # Should print 0.5
+print(fractional_part(5, 0)) # Should print 0
+print(fractional_part(0, 5)) # Should print 0
