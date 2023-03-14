457a305685aca5cde90c074c08486658220b0a35
Sra1995 <109440699+Sra1995@users.noreply.github.com>
2023-03-14 14:33:17 -0400

    Add files via upload

---
+++ parts_of_strings.py
@@ -0,0 +1,18 @@
+name = "Jaylen"
+print(name[1]) # a
+
+print(name[0]) # J
+
+print(name[5]) # error
+
+text = "Radnom string with a lot of characters"
+print(text[-1]) # s 
+
+print(text[-2]) # r
+
+fruit = "Pineapple"
+print(fruit[1:4]) #ine 
+
+print(fruit[:4])  # Pine 
+
+print(fruit[4:])  #apple
